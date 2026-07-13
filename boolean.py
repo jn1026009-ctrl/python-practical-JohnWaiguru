51e68d2b177b190fbefe87709f35b41f0eefbe7c
jn1026009-ctrl <jn1026009@gmail.com>
2026-07-13 23:13:56 +0300

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,13 @@
+# boolean.py
+is_logged_in = True
+has_premium_account = False
+
+x = 10
+y = 20
+
+print(f"Is {x} equal to {y} (x == y)? {x == y}")
+print(f"Is {x} not equal to {y} (x != y)? {x != y}")
+print(f"Is {x} greater than {y} (x > y)? {x > y}")
+print(f"Is {x} less than {y} (x < y)? {x < y}")
+print(f"Is {x} greater than or equal to {y} (x >= y)? {x >= y}")
+print(f"Is {x} less than or equal to {y} (x <= y)? {x <= y}")
