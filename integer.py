a8fd89c6dbd6ae9a0935c51ca9a88af4f674f509
jn1026009-ctrl <jn1026009@gmail.com>
2026-07-13 23:11:57 +0300

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,15 @@
+# integer.py
+# Creating integer variables
+a = 15
+b = 4
+
+print(f"Variable a: {a}")
+print(f"Variable b: {b}\n")
+
+# Performing arithmetic operations
+print(f"Addition (a + b): {a + b}")
+print(f"Subtraction (a - b): {a - b}")
+print(f"Multiplication (a * b): {a * b}")
+print(f"Division (a / b): {a / b}")
+print(f"Modulus/Remainder (a % b): {a % b}")
+print(f"Floor Division (a // b): {a // b}")
